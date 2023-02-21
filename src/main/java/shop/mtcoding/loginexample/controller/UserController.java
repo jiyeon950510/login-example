413d65b2f90407062be91d8252a9ae525eeca64b
jiyeon950510 <siki4682@naver.com>
2023-02-21 10:41:04 +0900

    회원가입 컨트롤러 완료

--- src/main/java/shop/mtcoding/loginexample/controller/UserController.java
+++ src/main/java/shop/mtcoding/loginexample/controller/UserController.java
@@ -1,14 +1,36 @@
 package shop.mtcoding.loginexample.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.loginexample.dto.JoinReqDto;
+import shop.mtcoding.loginexample.handler.ex.CustomException;
 
 @Controller
 public class UserController {
 
-    @GetMapping("logoutForm")
+    @PostMapping("/join")
+    public String join(JoinReqDto joinReqDto) {
+
+        if (joinReqDto.getUsername() == null || joinReqDto.getUsername().isEmpty()) {
+            throw new CustomException("Username을 입력해주세요", HttpStatus.BAD_REQUEST);
+        }
+        if (joinReqDto.getPassword() == null || joinReqDto.getPassword().isEmpty()) {
+            throw new CustomException("Password을 입력해주세요", HttpStatus.BAD_REQUEST);
+        }
+        if (joinReqDto.getEmail() == null || joinReqDto.getEmail().isEmpty()) {
+            throw new CustomException("Fullname 입력해주세요", HttpStatus.BAD_REQUEST);
+        }
+
+        return "redirect:/loginForm";
+    }
+
+    @GetMapping("/logout")
     public String logout() {
-        return "user/loginForm";
+        // session.invalidate();
+        return "redirect:/";
     }
 
     @GetMapping("loginForm")
---
+++ src/main/java/shop/mtcoding/loginexample/dto/LoginReqDto.java
@@ -0,0 +1,11 @@
+package shop.mtcoding.loginexample.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginReqDto {
+    private String username;
+    private String password;
+}
